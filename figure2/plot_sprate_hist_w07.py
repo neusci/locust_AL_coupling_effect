ccfea77a8f4d142c21adf53e858bfba0fc7f5a71
M W <mogeiwang@gmail.com>
2017-07-26 23:00:47 +0800

    add gain curve plotter

--- figure2/plot_sprate_hist_w07.py
+++ figure2/plot_sprate_hist_w07.py
@@ -32,8 +32,8 @@ def load_odor_sf_at_momment(m0,m1):
 x=load_resp_sf_at_momment(osc_bgn, osc_end)
 y=load_odor_sf_at_momment(osc_bgn, osc_end)
 if len(sList)==1:
-    savetxt('./data/hist_onNet_c%d.txt'%sList[0], x)
-    savetxt('./data/hist_offNet_c%d.txt'%sList[0], y)
+    savetxt('./data/hist_onNet_s%d.txt'%sList[0], x)
+    savetxt('./data/hist_offNet_s%d.txt'%sList[0], y)
 else:
     savetxt('./data/hist_onNet.txt', x)
     savetxt('./data/hist_offNet.txt', y)
@@ -43,8 +43,8 @@ hist(x, 20, normed=1, histtype='bar', color='b', alpha=0.75);
 xlabel('firing rate (Hz)')
 ylabel('fraction')
 if len(sList)==1:
-    savefig('hist_onNet_osc_c%d.jpg'%sList[0])
-    savefig('hist_onNet_osc_c%d.eps'%sList[0])
+    savefig('hist_onNet_osc_s%d.jpg'%sList[0])
+    savefig('hist_onNet_osc_s%d.eps'%sList[0])
 else:
     savefig('hist_onNet_osc.jpg')
     savefig('hist_onNet_osc.eps')
@@ -55,8 +55,8 @@ hist(y, 20, normed=1, histtype='bar', color='b', alpha=0.75);
 xlabel('firing rate (Hz)')
 ylabel('fraction')
 if len(sList)==1:
-    savefig('hist_offNet_osc_c%d.jpg'%sList[0])
-    savefig('hist_offNet_osc_c%d.eps'%sList[0])
+    savefig('hist_offNet_osc_s%d.jpg'%sList[0])
+    savefig('hist_offNet_osc_s%d.eps'%sList[0])
 else:
     savefig('hist_offNet_osc.jpg')
     savefig('hist_offNet_osc.eps')
@@ -71,7 +71,7 @@ prd_end2=3500
 
 z=load_resp_sf_at_momment(prd_bgn2, prd_end2)
 if len(sList)==1:
-    savetxt('./data/hist_onNet_period2_c%d.txt'%sList[0], z)
+    savetxt('./data/hist_onNet_period2_s%d.txt'%sList[0], z)
 else:
     savetxt('./data/hist_onNet_period2.txt', z)
 
@@ -79,8 +79,8 @@ hist(z, 20, normed=1, histtype='bar', color='b', alpha=0.75);
 xlabel('firing rate (Hz)')
 ylabel('fraction')
 if len(sList)==1:
-    savefig('hist_onNet_period2_c%d.jpg'%sList[0])
-    savefig('hist_onNet_period2_c%d.eps'%sList[0])
+    savefig('hist_onNet_period2_s%d.jpg'%sList[0])
+    savefig('hist_onNet_period2_s%d.eps'%sList[0])
 else:
     savefig('hist_onNet_period2.jpg')
     savefig('hist_onNet_period2.eps')
@@ -92,7 +92,7 @@ prd_end3=4500
 
 zz=load_resp_sf_at_momment(prd_bgn3, prd_end3)
 if len(sList)==1:
-    savetxt('./data/hist_onNet_period3_c%d.txt'%sList[0], zz)
+    savetxt('./data/hist_onNet_period3_s%d.txt'%sList[0], zz)
 else:
     savetxt('./data/hist_onNet_period3.txt', zz)
 
@@ -100,8 +100,8 @@ hist(zz, 20, normed=1, histtype='bar', color='b', alpha=0.75);
 xlabel('firing rate (Hz)')
 ylabel('fraction')
 if len(sList)==1:
-    savefig('hist_onNet_period3_c%d.jpg'%sList[0])
-    savefig('hist_onNet_period3_c%d.eps'%sList[0])
+    savefig('hist_onNet_period3_s%d.jpg'%sList[0])
+    savefig('hist_onNet_period3_s%d.eps'%sList[0])
 else:
     savefig('hist_onNet_period3.jpg')
     savefig('hist_onNet_period3.eps')
