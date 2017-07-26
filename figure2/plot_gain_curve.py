340a387dbde6a1e0497a86341e0d27302796da80
M W <mogeiwang@gmail.com>
2017-07-27 01:42:07 +0800

    add PN selectivity plotter

--- figure2/plot_gain_curve.py
+++ figure2/plot_gain_curve.py
@@ -8,6 +8,7 @@ offnet=array([load_sf_avged_over_trial(100,i,1100,1350,50) for i in slist])
 onnet =array([load_sf_avged_over_trial(  0,i,1100,1350,50) for i in slist])
 
 
+# this is for plot gain curve, not good
 for i in range(PN_number):
     print(i)
     plot(offnet[:,i], onnet[:,i], '.');
@@ -27,4 +28,5 @@ savefig('off2onnet_gain_curve_all.jpg')
 savefig('off2onnet_gain_curve_all.eps')
 savefig('./fig/off2onnet_gain_curve_all.jpg')
 savefig('./fig/off2onnet_gain_curve_all.eps')
-show()
+clf()
+#show()
